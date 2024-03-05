34b0f0b1bdb2450e410a3b627de37ef2656a8966
Sacha <sacha.mutschler@epitech.eu>
2024-03-05 17:05:43 +0100

    feat: creating button and input component

---
+++ src/components/texts/PrimaryTitle/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+export const PrimaryTitle = styled.Text`
+    font-size: 32px;
+    color: black;
+    text-align: center;
+    margin: 10px auto 10px auto;
+`;
--- src/screens/GalleryScreen.tsx
+++ src/screens/GalleryScreen.tsx
@@ -1,5 +1,14 @@
 import { Text } from "react-native";
+import PrimaryButton from "../components/buttons/PrimaryButton"
+import {FormInputStyle} from "../components/inputs/PrimaryInput/style";
+import {PrimaryTitle} from "../components/texts/PrimaryTitle/style";
 
 export default function GalleryScreen() {
-  return <Text>GalleryScreen</Text>;
+  return (
+      <>
+        <PrimaryTitle> Title </PrimaryTitle>
+        <FormInputStyle placeholder={'email'} />
+        <PrimaryButton onPress={() => console.log('test')} text={'Log in'} />
+      </>
+  )
 }
