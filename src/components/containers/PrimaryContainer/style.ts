95926fa9a394849188a50a585f8dfe615302d335
SachaMutschler <sacha.mutschler@ati4group.com>
2024-03-19 13:57:25 +0100

    DRAFT: creating camera screen

--- src/components/containers/PrimaryContainer/style.ts
+++ src/components/containers/PrimaryContainer/style.ts
@@ -11,3 +11,13 @@ export const ItemContainerStyle = styled.View`
     width: 100%;
     height: 100%;
 `;
+
+export const CameraContainerStyle = styled.View`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+`;
