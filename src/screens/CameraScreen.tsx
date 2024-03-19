1359a904c089d0c7aefad0d6968125d870120bf4
SachaMutschler <sacha.mutschler@ati4group.com>
2024-03-19 13:57:25 +0100

    DRAFT: creating camera screen

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,5 +1,43 @@
-import { Text } from "react-native";
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {FormInputStyle} from "../components/inputs/PrimaryInput/style";
+import {CameraContainerStyle} from "../components/containers/PrimaryContainer/style";
 
-export default function CameraScreen() {
-  return <Text>CameraScreen</Text>;
+export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (permission?.status === 'undetermined') {
+    return (
+      <CameraContainerStyle>
+        <Text>Requesting camera permission</Text>
+        <Button title="Request permission" onPress={() => requestPermission()} />
+      </CameraContainerStyle>
+    );
+  }
+
+    if (permission?.status !== 'granted') {
+        return (
+          <CameraContainerStyle>
+              <Text>Camera permission not granted</Text>
+          </CameraContainerStyle>
+        );
+    }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
+  return (
+      <CameraContainerStyle>
+        <Camera style={{ flex: 1 }} type={type}>
+          <CameraContainerStyle>
+            <TouchableOpacity onPress={toggleCameraType}>
+              <FormInputStyle>Flip Camera</FormInputStyle>
+            </TouchableOpacity>
+          </CameraContainerStyle>
+        </Camera>
+      </CameraContainerStyle>
+  );
 }
