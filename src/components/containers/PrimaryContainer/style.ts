6504729dd8342af12a776ea3fe898f1e00b74146
Sacha <sacha.mutschler@epitech.eu>
2024-03-25 10:53:01 +0100

    feat: creating camera screen

--- src/components/containers/PrimaryContainer/style.ts
+++ src/components/containers/PrimaryContainer/style.ts
@@ -14,10 +14,8 @@ export const ItemContainerStyle = styled.View`
 
 export const CameraContainerStyle = styled.View`
     display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    align-items: center;
     justify-content: center;
+    align-items: center;
     width: 100%;
     height: 100%;
 `;
