a6ec24d1a183b8c106d3fbc2702be16319acc210
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-29 19:42:59 +0100

    fix(url): not 8000 but 8080

--- src/api/index.api.ts
+++ src/api/index.api.ts
@@ -2,19 +2,23 @@ import axios from "axios";
 
 const AUTH_TOKEN = "";
 
+const API_URL = "http://localhost:8080";
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: `${API_URL}/api`,
     headers: {
         "Content-Type": "application/json",
     },
 });
 
 export const axiosFiles = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: `${API_URL}/api`,
     headers: {
         "Content-Type": "multipart/form-data",
     },
 });
+
+export const checkHealth = async () => axios.get(`${API_URL}/health-check`);
 
 axiosInstance.defaults.headers.common["Authorization"] = AUTH_TOKEN;
 axiosInstance.defaults.timeout = 3000;
