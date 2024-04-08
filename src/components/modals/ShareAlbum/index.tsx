313d4e860ef5e593653532798499c796b9054d83
Sacha <sacha.mutschler@epitech.eu>
2024-04-08 16:32:14 +0200

    feat: creating update and share modals

--- src/components/modals/ShareAlbum/index.tsx
+++ src/components/modals/ShareAlbum/index.tsx
@@ -1,61 +1,62 @@
-import React, {useState} from "react";
-import {Modal, TouchableWithoutFeedback, View} from 'react-native';
-import {ModalContainerStyle, ViewStyle} from "./style";
-import {FormInputStyle} from "../../inputs/PrimaryInput/style";
-import PrimaryButton from "../../buttons/PrimaryButton";
-import {AlbumImageStyle} from "../../buttons/Album/style";
+import React, { useState } from 'react';
+import {Modal, TouchableWithoutFeedback, View} from "react-native";
+import {ModalContainerStyle} from "../AddAlbum/style";
 import {PrimaryTitle} from "../../texts/PrimaryTitle/style";
+import {AlbumImageStyle} from "../../buttons/Album/style";
+import {FormInputStyle} from "../../inputs/PrimaryInput/style";
 import {ManageError} from "../../texts/PrimaryText";
+import PrimaryButton from "../../buttons/PrimaryButton";
 import {useDispatch, useSelector} from "react-redux";
-import {setAddModalVisible} from "../../../store/slices/albumSlice";
+import {setShareModalVisible, setUpdateModalVisible} from "../../../store/slices/albumSlice";
 
-function AddAlbumModal() {
+export const ShareAlbum: React.FC = () => {
     const dispatch = useDispatch();
-    const isModalVisible = useSelector((state: any) => state.album.isAddModalVisible);
-    const [albumName, setAlbumName] = useState('');
+    const modalVisible = useSelector((state: any) => state.album.isShareModalVisible);
+    const [email, setEmail] = useState('');
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const handleAddAlbum = () => {
-        if (albumName === '') {
+    const handleUpdateAlbum = () => {
+        if (email === '') {
             setError(true);
-            setErrorMessage('Album name is required');
+            setErrorMessage('Email is required');
             return;
         }
+
         // @todo call api to add album
-        console.log('Add Album');
+        console.log('Share Album');
     };
 
     const closeModal = () => {
-        dispatch(setAddModalVisible(false));
-    };
+        dispatch(setShareModalVisible(false));
+    }
 
     return (
         <Modal
             animationType="slide"
             transparent={true}
-            visible={isModalVisible}
+            visible={modalVisible}
             onRequestClose={() => closeModal()}
         >
             <TouchableWithoutFeedback onPress={() => closeModal()}>
                 <View style={{flex: 1}}>
                     <TouchableWithoutFeedback onPress={(event) => event.stopPropagation()}>
                         <ModalContainerStyle>
-                            <PrimaryTitle>Add Album</PrimaryTitle>
+                            <PrimaryTitle>Share Album</PrimaryTitle>
                             <AlbumImageStyle
                                 source={require("../../../../assets/images/album_icon.png")}
                             />
                             <FormInputStyle
-                                onChangeText={name => setAlbumName(name)}
-                                placeholder={"Name"}
+                                onChangeText={value => setEmail(value)}
+                                placeholder={"Email"}
                             />
                             <ManageError
                                 error={error}
                                 errorMessage={errorMessage}
                             />
                             <PrimaryButton
-                                onPress={() => handleAddAlbum()}
-                                text={"Create"}
+                                onPress={() => handleUpdateAlbum()}
+                                text={'Share'}
                             />
                         </ModalContainerStyle>
                     </TouchableWithoutFeedback>
@@ -65,4 +66,4 @@ function AddAlbumModal() {
     );
 }
 
-export default AddAlbumModal;
+export default ShareAlbum;
