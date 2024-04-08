8af6a4c81127e6a30860646a5bc8bfdfc324f453
Nicolas Delplanque <nicolas@delplanque.eu>
2024-04-08 10:25:24 +0200

    feat(CameraScreen): added createPhoto

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
-import {Button, Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useState} from 'react';
+import {Button, Modal} from 'react-native';
 import {CameraContainerStyle} from "../components/containers/PrimaryContainer/style";
 import * as ImagePicker from 'expo-image-picker';
 import {ViewStyle} from "../components/modals/AddAlbum/style";
+import {createPhoto} from "../api/photo.api";
 
 export default function App() {
-    const [imageUri, setImageUri] = useState('');
+    const [imageUri, setImageUri] = useState<string | undefined>();
     const [modalVisible, setModalVisible] = useState(true);
 
     const openImagePicker = async (useCamera: boolean) => {
@@ -28,24 +29,25 @@ export default function App() {
 
         if (!result.canceled && 'uri' in result) {
             setImageUri(result.uri as string);
+            await createPhoto(result.uri as string);
         }
     };
 
-  return (
-      <CameraContainerStyle>
-          <Modal
-              animationType="slide"
-              transparent={false}
-              visible={modalVisible}
-              onRequestClose={() => {
-                  setModalVisible(!modalVisible);
-              }}>
-              <ViewStyle>
-                  <Button title="Take a Picture" onPress={() => openImagePicker(true)}/>
-                  <Button title="Choose from Gallery" onPress={() => openImagePicker(false)}/>
-                  <Button title="Cancel" onPress={() => setModalVisible(false)}/>
-              </ViewStyle>
-          </Modal>
-      </CameraContainerStyle>
-  );
+    return (
+        <CameraContainerStyle>
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+                <ViewStyle>
+                    <Button title="Take a Picture" onPress={() => openImagePicker(true)}/>
+                    <Button title="Choose from Gallery" onPress={() => openImagePicker(false)}/>
+                    <Button title="Cancel" onPress={() => setModalVisible(false)}/>
+                </ViewStyle>
+            </Modal>
+        </CameraContainerStyle>
+    );
 }
