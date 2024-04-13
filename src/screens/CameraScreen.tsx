d80a2fa76778e998399a5bcb33c3d6b7e4ead077
Nicolas Delplanque <nicolas@delplanque.eu>
2024-04-13 07:49:18 +0200

    fix(indentation): sorry for trolling

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -6,56 +6,56 @@ import {ViewStyle} from "../components/modals/AddAlbum/style";
 import {createPhoto} from "../api/photo.api";
 
 export default function App() {
-    const [imageUri, setImageUri] = useState<string | undefined>();
-    const [modalVisible, setModalVisible] = useState(true);
+  const [imageUri, setImageUri] = useState<string | undefined>();
+  const [modalVisible, setModalVisible] = useState(true);
 
-    useEffect(() => {
-        if (imageUri) {
-            createPhoto(imageUri, ['todo']);
-        }
-    }, [imageUri]);
+  useEffect(() => {
+    if (imageUri) {
+      createPhoto(imageUri, ['todo']);
+    }
+  }, [imageUri]);
 
-    const openImagePicker = async (useCamera: boolean) => {
-        let result;
+  const openImagePicker = async (useCamera: boolean) => {
+    let result;
 
-        if (useCamera) {
-            result = await ImagePicker.launchCameraAsync({
-                allowsEditing: true,
-                aspect: [4, 3],
-                quality: 1,
-            });
-        } else {
-            result = await ImagePicker.launchImageLibraryAsync({
-                mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                allowsEditing: true,
-                aspect: [4, 3],
-                quality: 1,
-            });
-        }
+    if (useCamera) {
+      result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+    } else {
+      result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+    }
 
-        if (!result.canceled && result.assets?.[0]?.uri) {
-            setImageUri(result.assets?.[0]?.uri);
-            console.log("uri: " + result.assets?.[0]?.uri);
-        }
+    if (!result.canceled && result.assets?.[0]?.uri) {
+      setImageUri(result.assets?.[0]?.uri);
+      console.log("uri: " + result.assets?.[0]?.uri);
+    }
 
-        console.log("result: " + JSON.stringify(result));
-    };
+    console.log("result: " + JSON.stringify(result));
+  };
 
-    return (
-        <CameraContainerStyle>
-            <Modal
-                animationType="slide"
-                transparent={false}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                }}>
-                <ViewStyle>
-                    <Button title="Take a Picture" onPress={() => openImagePicker(true)}/>
-                    <Button title="Choose from Gallery" onPress={() => openImagePicker(false)}/>
-                    <Button title="Cancel" onPress={() => setModalVisible(false)}/>
-                </ViewStyle>
-            </Modal>
-        </CameraContainerStyle>
-    );
+  return (
+    <CameraContainerStyle>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <ViewStyle>
+          <Button title="Take a Picture" onPress={() => openImagePicker(true)}/>
+          <Button title="Choose from Gallery" onPress={() => openImagePicker(false)}/>
+          <Button title="Cancel" onPress={() => setModalVisible(false)}/>
+        </ViewStyle>
+      </Modal>
+    </CameraContainerStyle>
+  );
 }
