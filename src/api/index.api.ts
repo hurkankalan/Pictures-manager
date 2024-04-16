25a9c06933764046ddd7a1c3a3cbb905e1b1498b
Nicolas Delplanque <nicolas@delplanque.eu>
2024-04-16 17:21:51 +0200

    feat(search): added photo search feature and screen

--- src/api/index.api.ts
+++ src/api/index.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://192.168.28.150:8080";
+const API_URL = "http://10.22.255.148:8080";
 
 const axiosInstance = axios.create({
   baseURL: `${API_URL}/api`,
--- src/screens/Gallery/GalleryNavigator.tsx
+++ src/screens/Gallery/GalleryNavigator.tsx
@@ -4,6 +4,7 @@ import PhotoScreen, {PhotoScreenProps} from "./PhotoScreen";
 import AlbumScreen, {AlbumScreenProps} from "./AlbumScreen";
 import GalleryScreen from "./GalleryScreen";
 import {RootStackParamList} from "../../navigation/navigation.types";
+import SearchScreen, {SearchScreenProps} from "./SearchScreen";
 
 const Gallery = createNativeStackNavigator<RootStackParamList>();
 
@@ -13,9 +14,19 @@ export default function GalleryNavigator() {
       <Gallery.Screen name={"GalleryHome"} component={GalleryScreen} options={{
         title: 'Albums'
       }}/>
-      <Gallery.Screen name={"Album"} component={AlbumScreen} options={({route}: AlbumScreenProps) => ({
-        title: 'Album ' + route.params.album.id
-      })}/>
+      <Gallery.Screen name={"Album"} component={AlbumScreen} options={
+        ({route, navigation}: AlbumScreenProps) => ({
+          title: 'Album ' + route.params.album.id,
+          headerSearchBarOptions: {
+            placeholder: "Search"
+          },
+        })
+      }/>
+      <Gallery.Screen name={"Search"} component={SearchScreen} options={
+        ({route, navigation}: SearchScreenProps) => ({
+          title: route.params.search,
+        })
+      }/>
       <Gallery.Screen name={"Photo"} component={PhotoScreen} options={({route}: PhotoScreenProps) => ({
         title: 'Photo ' + route.params.photo.id
       })}/>
